d8283732648607d105c69ce395e4b9303aa6732f
knnisha <nisha.devops25@gmail.com>
2025-10-31 03:54:40 +0600

    Deleted this one to avoid any kind of confusion

--- scripts/deploy-elasticsearch.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd ../03-helm/elasticsearch
-helm install customer-abc-elasticsearch . -f envs/qa/customer-abc-elasticsearch/values.yaml -n default
-kubectl wait --for=condition=ready pod -l app=customer-abc-elasticsearch -n default --timeout=300s || true
-kubectl get pods,svc,statefulset -n default
--- scripts/test-docker-nginx.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd ../02-docker-kubernetes/docker-compose
-docker-compose up -d
-sleep 5
-docker-compose ps
-curl -s http://localhost | head -n 5
-docker-compose logs nginx | tail -n 10
